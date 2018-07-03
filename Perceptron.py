169aaa2ab9a6c82e28d39ab36d2a7f235e96dce2
Kevin O'Brien <kevob33@gmail.com>
2018-07-03 10:33:07 +0400

    update

--- Perceptron.py
+++ Perceptron.py
@@ -1,10 +1,6 @@
 import numpy as np
 
-learning_rate = 0.01
-num_inputs = 3
-data_set_size = 100
-num_epochs = 100
-print_step = 10
+import Data
 
 
 class Perceptron:
@@ -13,55 +9,46 @@ class Perceptron:
         self.num_inputs = num_inputs
         self.weights = np.random.random((num_inputs, 1))
         self.bias = np.random.random()
-        self.w_gradients = np.zeros_like(self.weights)
-        self.b_gradient = np.zeros_like(self.bias)
+        self.w_gradients = np.zeros_like(self.weights)      # Changes to be made to weights and bias
+        self.b_gradient = np.zeros_like(self.bias)          # calculated through backprop
 
-    def sigmoid(self, x):
+    def sigmoid(self, x, derivative=False):
+        if derivative:
+            return x * (1 - x)
         return 1 / (1 + np.exp(-x))
 
-    def sigmoid_derivative(self, x):
-        return x * (1 - x)
-
     def forward(self, inputs):
-        self.inputs = np.array(inputs).reshape(self.num_inputs, 1)
-        weighted_sum = np.dot(inputs, self.weights) + self.bias
-        self.prediction = self.sigmoid(weighted_sum)
+        self.inputs = np.array(inputs).reshape(self.num_inputs, 1)  # Python list -> Numpy array, (3,) -> (3, 1)
+        weighted_sum = np.dot(inputs, self.weights) + self.bias     # xW + b
+        self.prediction = self.sigmoid(weighted_sum)                # Activation
         return self.prediction
 
     def backward(self, label):
-        loss = (label - self.prediction) ** 2
-        delta = 2 * (label - self.prediction) * self.sigmoid_derivative(self.prediction)
-        self.w_gradients += self.inputs.dot(delta).reshape(self.num_inputs, 1)
-        self.b_gradient += delta[0]
+        loss = (label - self.prediction) ** 2   # Squared Error
+        delta = 2 * (label - self.prediction) * self.sigmoid(self.prediction, derivative=True)  # Output delta
+        self.w_gradients += self.inputs.dot(delta).reshape(self.num_inputs, 1)                  # Changes to be ade to weights
+        self.b_gradient += delta[0]     # Change to be made to bias - (1,1) matrix -> single digit
         return loss
 
     def update(self, learning_rate):
         self.weights += learning_rate * self.w_gradients
         self.bias += learning_rate * self.b_gradient
-        self.w_gradients = np.zeros_like(self.weights)
+        self.w_gradients = np.zeros_like(self.weights)      # Reset changes to zero after update
         self.b_gradient = np.zeros_like(self.bias)
 
 
-# Input: num_inputs numbers between -10 and +10
-# Label: 1 if sum of inputs > 0, otherwise 0
-inputs, labels = [], []
-for i in range(data_set_size):
-    input = np.random.uniform(-10, 10, num_inputs)
-    inputs.append(input)
-    labels.append((np.sum(input) > 0).astype(int))
+perceptron = Perceptron(Data.num_inputs)
 
-perceptron = Perceptron(num_inputs)
-
-for i in range(num_epochs):
+for i in range(Data.num_epochs):
     epoch_loss = 0
     epoch_correct = []
-    for j in range(len(inputs)):
-        prediction = perceptron.forward(inputs[j])
-        loss = perceptron.backward(labels[j])
+    for j in range(len(Data.inputs)):
+        prediction = perceptron.forward(Data.inputs[j])
+        loss = perceptron.backward(Data.labels[j])
         epoch_loss += loss
-        epoch_correct.append(np.round(prediction) == labels[j])
-    perceptron.update(learning_rate)
-    if i == 0 or (i + 1) % print_step == 0:
+        epoch_correct.append(np.round(prediction) == Data.labels[j])
+    perceptron.update(Data.learning_rate)
+    if i == 0 or (i + 1) % Data.print_step == 0:
         print("Epoch:", i + 1)
-        print("Error:", (epoch_loss / len(inputs))[0])
-        print("Acc  :", (np.sum(epoch_correct) / len(inputs)) * 100, "%\n")
+        print("Error:", (epoch_loss / len(Data.inputs))[0])
+        print("Acc  :", (np.sum(epoch_correct) / len(Data.inputs)) * 100, "%\n")
--- PerceptronTF.py
+++ PerceptronTF.py
@@ -1,22 +1,18 @@
 import numpy as np
 import tensorflow as tf
 
-learning_rate = 0.01
-num_inputs = 3
-data_set_size = 100
-num_epochs = 100
-print_step = 10
+import Data
 
 inputs, labels = [], []
-for i in range(data_set_size):
-    input = np.random.uniform(-10, 10, num_inputs)
+for i in range(Data.data_set_size):
+    input = np.random.uniform(-10, 10, Data.num_inputs)
     inputs.append([input])
     labels.append([(np.sum(input) > 0).astype(int)])
 
-x = tf.placeholder(tf.float32, [1, num_inputs])
+x = tf.placeholder(tf.float32, [1, Data.num_inputs])
 y = tf.placeholder(tf.float32, [1])
 
-weights = tf.Variable(tf.random_normal([num_inputs, 1]))
+weights = tf.Variable(tf.random_normal([Data.num_inputs, 1]))
 bias = tf.Variable(tf.random_normal([1]))
 
 weighted_sum = tf.matmul(x, weights) + bias
@@ -24,18 +20,18 @@ prediction = tf.sigmoid(weighted_sum)[0]
 
 loss = tf.losses.mean_squared_error(y, prediction)
 
-train = tf.train.AdamOptimizer(learning_rate).minimize(loss)
+train = tf.train.AdamOptimizer(Data.learning_rate).minimize(loss)
 
 with tf.Session() as session:
     session.run(tf.global_variables_initializer())
-    for i in range(num_epochs):
+    for i in range(Data.num_epochs):
         epoch_loss = 0
         epoch_correct = []
         for j in range(len(inputs)):
             _, l, pred, label = session.run([train, loss, prediction, y], {x: inputs[j], y: labels[j]})
             epoch_loss += l
             epoch_correct.append(np.round(pred) == label)
-        if i == 0 or (i + 1) % print_step == 0:
+        if i == 0 or (i + 1) % Data.print_step == 0:
             print("Epoch:", i + 1)
             print("Error:", (epoch_loss / len(inputs)))
             print("Acc  :", (np.sum(epoch_correct) / len(inputs)) * 100, "%\n")
